492d9053b3f58574ad80d07c07b32f5b0d797ac5
yamam00s <s.yamamo0327@gmail.com>
2021-05-24 02:28:17 +0900

    resolvers 型定義適応

--- server/src/resolvers/index.ts
+++ server/src/resolvers/index.ts
@@ -1,14 +1,18 @@
-export const resolvers = {
-  Query: {
-    contents: (_, __, { dataSources }) =>
-      dataSources.microCmsRestApi.fetchAllContents(),
-    singleContentsById: (_, { id }, { dataSources }) =>
-      dataSources.microCmsRestApi.fetchSingleContentsById({ id }),
-    arranged: (_, __, { dataSources }) =>
-      dataSources.microCmsRestApi.fetchArranged(),
-    favorites: (_, __, { dataSources }) =>
-      dataSources.microCmsRestApi.fetchFavorites(),
-    favoritesByCategoryIds: (_, __, { dataSources }) =>
+import { QueryResolvers } from '../generated/graphql';
+
+const Query: QueryResolvers = {
+  contents: (_, __, { dataSources }) =>
+    dataSources.microCmsRestApi.fetchAllContents(),
+  singleContentsById: (_, { id }, { dataSources }) =>
+    dataSources.microCmsRestApi.fetchSingleContentsById({ id }),
+  arranged: (_, __, { dataSources }) =>
+    dataSources.microCmsRestApi.fetchArranged() as string,
+  favorites: (_, __, { dataSources }) =>
+    dataSources.microCmsRestApi.fetchFavorites(),
+  favoritesByCategoryIds: (_, __, { dataSources }) =>
     dataSources.microCmsRestApi.fetchFavoritesByCategoryIds()
-  }
+}
+
+export const resolvers = {
+  Query
 }
