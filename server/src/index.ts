c7650453cebf92ccafc8ca5c88ac343b4a2e3da0
yamam00s <s.yamamo0327@gmail.com>
2021-05-25 09:48:38 +0900

    add apollo-server-lambda

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,5 @@
-import { ApolloServer } from 'apollo-server'
+// import { ApolloServer } from 'apollo-server'
+import { ApolloServer } from 'apollo-server-lambda'
 import { typeDefs } from './schema'
 import { resolvers } from './resolvers'
 import { MicroCmsRestApi } from './datasources/MicroCmsRestApi'
@@ -12,6 +13,13 @@ const server = new ApolloServer({
 })
 
 // The `listen` method launches a web server.
-server.listen().then(async({ url }: { url: string }) => {
-  console.log(`🚀  Server ready at ${url}`)
+// server.listen().then(async({ url }: { url: string }) => {
+//   console.log(`🚀  Server ready at ${url}`)
+// })
+
+exports.handler = server.createHandler({
+  cors: {
+    origin: '*',
+    credentials: true,
+  }
 })
