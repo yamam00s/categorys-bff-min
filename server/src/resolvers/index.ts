3c0ab3d4c2621010bc4d7bff5747fa58831738d9
yamam00s <s.yamamo0327@gmail.com>
2021-06-18 02:21:36 +0900

    QueryResolversの型定義

--- server/src/resolvers/index.ts
+++ server/src/resolvers/index.ts
@@ -1,15 +1,21 @@
 import { QueryResolvers } from '../generated/graphql';
+import { MicroCmsRestApi } from '../datasources/MicroCmsRestApi'
 
+type DataSources = {
+  dataSources: {
+    microCmsRestApi: MicroCmsRestApi
+  }
+}
 const Query: QueryResolvers = {
-  contents: (_, __, { dataSources }) =>
+  contents: (_, __, { dataSources }: DataSources) =>
     dataSources.microCmsRestApi.fetchAllContents(),
-  singleContentsById: (_, { id }, { dataSources }) =>
+  singleContentsById: (_, { id }, { dataSources }: DataSources) =>
     dataSources.microCmsRestApi.fetchSingleContentsById({ id }),
-  arranged: (_, __, { dataSources }) =>
+  arranged: (_, __, { dataSources }: DataSources) =>
     dataSources.microCmsRestApi.fetchArranged(),
-  favorites: (_, __, { dataSources }) =>
+  favorites: (_, __, { dataSources }: DataSources) =>
     dataSources.microCmsRestApi.fetchFavorites(),
-  favoritesByCategoryIds: (_, __, { dataSources }) =>
+  favoritesByCategoryIds: (_, __, { dataSources }: DataSources) =>
     dataSources.microCmsRestApi.fetchFavoritesByCategoryIds()
 }
 
