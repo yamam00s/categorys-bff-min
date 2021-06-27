415d3a6febf301574ca7ed8d044eced1af25af5e
yamam00s <s.yamamo0327@gmail.com>
2021-06-28 02:21:09 +0900

    spyを使ったHooksのテスト fix

--- client/src/hooks/useInitialAnalytics.test.ts
+++ client/src/hooks/useInitialAnalytics.test.ts
@@ -3,7 +3,7 @@ import { useInitialAnalytics } from './useInitialAnalytics'
 import * as analyticsModule from "../libs/analytics"
 
 const spiedSetStatus = jest.spyOn(analyticsModule, "setStatus")
-const spiedSendPageView = jest.spyOn(analyticsModule, "setStatus")
+const spiedSendPageView = jest.spyOn(analyticsModule, "sendPageview")
 
 test("useInitialAnalytics 初回実行", () => {
   renderHook(() => useInitialAnalytics({ id: "foo", role: "bar" }))
