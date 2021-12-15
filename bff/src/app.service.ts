e7f399562405012ae1250d6539f33c612a661036
yamam00s <s.yamamo0327@gmail.com>
2021-12-16 01:13:54 +0900

    service→controller

--- bff/src/app.service.ts
+++ bff/src/app.service.ts
@@ -1,8 +1,34 @@
 import { Injectable } from '@nestjs/common';
 
+export interface Item {
+  id: number;
+  title: string;
+  body: string;
+  deletePassword: string;
+}
+
+export type PublicItem = Omit<Item, 'deletePassword'>;
+
+const items: Item[] = [
+  {
+    id: 1,
+    title: 'Item Title',
+    body: 'Hello, world',
+    deletePassword: '1234',
+  },
+];
+
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private getAllItem(): Item[] {
+    return [...items];
+  }
+
+  getPublicItems(): PublicItem[] {
+    return this.getAllItem().map((item) => {
+      const publicItem = { ...item };
+      delete publicItem.deletePassword;
+      return publicItem;
+    });
   }
 }
